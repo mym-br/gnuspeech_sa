mym-br/gnuspeech_sa

commit 7789b795da8f7597d841e339632b449af050ea1e

Removed VS2013 warnings.

src/trm_control_model/EventList.cpp
@@ -107,7 +107,7 @@ EventList::setUp()
 }
 
 void
-EventList::setUpDriftGenerator(float deviation, float sampleRate, float lowpassCutoff)
+EventList::setUpDriftGenerator(double deviation, double sampleRate, double lowpassCutoff)
 {
 	driftGenerator_.setUp(deviation, sampleRate, lowpassCutoff);
 }
@@ -1013,19 +1013,19 @@ EventList::generateOutput(std::ostream& trmParamStream)
 		currentValues[32] = -20.0;
 	}
 
-	unsigned int i = 1;
+	unsigned int index = 1;
 	int currentTime = 0;
 	int nextTime = list_[1]->time;
-	while (i < list_.size()) {
+	while (index < list_.size()) {
 
 		for (int j = 0; j < 16; j++) {
 			table[j] = (float) currentValues[j] + (float) currentValues[j + 16];
 		}
 		if (!microFlag_) table[0] = 0.0;
-		if (driftFlag_)  table[0] += driftGenerator_.drift();
-		if (macroFlag_)  table[0] += currentValues[32];
+		if (driftFlag_)  table[0] += static_cast<float>(driftGenerator_.drift());
+		if (macroFlag_)  table[0] += static_cast<long>(currentValues[32]);
 
-		table[0] += pitchMean_;
+		table[0] += static_cast<float>(pitchMean_);
 
 		trmParamStream << std::fixed << std::setprecision(3);
 		trmParamStream << table[0];
@@ -1056,14 +1056,14 @@ EventList::generateOutput(std::ostream& trmParamStream)
 		currentTime += 4;
 
 		if (currentTime >= nextTime) {
-			i++;
-			if (i == list_.size()) {
+			++index;
+			if (index == list_.size()) {
 				break;
 			}
-			nextTime = list_[i]->time;
+			nextTime = list_[index]->time;
 			for (int j = 0; j < 33; j++) { /* 32? 33? */
-				if (list_[i - 1]->getValue(j) != GS_EVENTLIST_INVALID_EVENT_VALUE) {
-					unsigned int k = i;
+				if (list_[index - 1]->getValue(j) != GS_EVENTLIST_INVALID_EVENT_VALUE) {
+					unsigned int k = index;
 					while ((temp = list_[k]->getValue(j)) == GS_EVENTLIST_INVALID_EVENT_VALUE) {
 						if (k >= list_.size() - 1U) {
 							currentDeltas[j] = 0.0;
@@ -1078,12 +1078,12 @@ EventList::generateOutput(std::ostream& trmParamStream)
 				}
 			}
 			if (smoothIntonation_) {
-				if (list_[i - 1]->getValue(33) != GS_EVENTLIST_INVALID_EVENT_VALUE) {
-					currentValues[32] = list_[i - 1]->getValue(32);
+				if (list_[index - 1]->getValue(33) != GS_EVENTLIST_INVALID_EVENT_VALUE) {
+					currentValues[32] = list_[index - 1]->getValue(32);
 					currentDeltas[32] = 0.0;
-					currentDeltas[33] = list_[i - 1]->getValue(33);
-					currentDeltas[34] = list_[i - 1]->getValue(34);
-					currentDeltas[35] = list_[i - 1]->getValue(35);
+					currentDeltas[33] = list_[index - 1]->getValue(33);
+					currentDeltas[34] = list_[index - 1]->getValue(34);
+					currentDeltas[35] = list_[index - 1]->getValue(35);
 				}
 			}
 		}

CMakeLists.txt
@@ -17,6 +17,9 @@ set(GNUSPEECHSA_VERSION_STRING ${GNUSPEECHSA_VERSION_MAJOR}.${GNUSPEECHSA_VERSIO
 if(MSVC)
     # To have M_PI defined in <cmath>.
     add_definitions(-D_USE_MATH_DEFINES)
+
+    # Disable warnings about strcpy_s, strcat_s, etc.
+    add_definitions(-D_CRT_SECURE_NO_DEPRECATE)
 endif()
 
 include_directories(

src/WAVEFileWriter.cpp
@@ -63,8 +63,8 @@ WAVEFileWriter::writeWaveFileHeader(int channels, int numberSamples, float outpu
 {
 	int dataChunkSize = channels * numberSamples * sizeof(std::int16_t);
 	int formSize = 4 + 24 + (8 + dataChunkSize);
-	int frameSize = std::ceil(channels * (BITS_PER_SAMPLE / 8.0));
-	int bytesPerSecond = std::ceil(outputRate * frameSize);
+	int frameSize = static_cast<int>(std::ceil(channels * (BITS_PER_SAMPLE / 8.0)));
+	int bytesPerSecond = static_cast<int>(std::ceil(outputRate * frameSize));
 
 	/*  Form container identifier  */
 	fputs("RIFF", stream_);
@@ -88,7 +88,7 @@ WAVEFileWriter::writeWaveFileHeader(int channels, int numberSamples, float outpu
 	writeUInt16LE(channels);
 
 	/*  Output Sample Rate  */
-	writeUInt32LE(std::round(outputRate));
+	writeUInt32LE(static_cast<int>(std::round(outputRate)));
 
 	/*  Bytes per second  */
 	writeUInt32LE(bytesPerSecond);
@@ -121,7 +121,7 @@ WAVEFileWriter::writeWaveFileHeader(int channels, int numberSamples, float outpu
 void
 WAVEFileWriter::writeSample(float sample)
 {
-	writeUInt16LE(std::round(sample * sampleScale_));
+	writeUInt16LE(static_cast<int>(std::round(sample * sampleScale_)));
 }
 
 /******************************************************************************
@@ -139,8 +139,8 @@ WAVEFileWriter::writeSample(float sample)
 void
 WAVEFileWriter::writeStereoSamples(float leftSample, float rightSample)
 {
-	writeUInt16LE(std::round(leftSample * sampleScale_));
-	writeUInt16LE(std::round(rightSample * sampleScale_));
+	writeUInt16LE(static_cast<int>(std::round(leftSample * sampleScale_)));
+	writeUInt16LE(static_cast<int>(std::round(rightSample * sampleScale_)));
 }
 
 /******************************************************************************

src/en/text_parser/TextParser.cpp
@@ -484,7 +484,7 @@ strip_punctuation(char* buffer, int length, std::stringstream& stream, long* str
 	}
 
 	/*  SET STREAM LENGTH  */
-	*stream_length = stream.tellp();
+	*stream_length = static_cast<long>(stream.tellp());
 }
 
 /******************************************************************************
@@ -687,7 +687,7 @@ set_tone_group(std::stringstream& stream, long tg_pos, const char* word)
 	}
 
 	/*  GET CURRENT POSITION IN STREAM  */
-	long current_pos = stream.tellp();
+	long current_pos = static_cast<long>(stream.tellp());
 
 	/*  SEEK TO TONE GROUP MARKER POSITION  */
 	stream.seekp(tg_pos);
@@ -754,7 +754,7 @@ convert_silence(const char* buffer, std::stringstream& stream)
 	}
 
 	/*  RETURN ACTUAL LENGTH OF SILENCE  */
-	return number_silence_phones * SILENCE_PHONE_LENGTH;
+	return static_cast<float>(number_silence_phones * SILENCE_PHONE_LENGTH);
 }
 
 /******************************************************************************
@@ -879,7 +879,7 @@ insert_tag(std::stringstream& stream, long insert_point, const char* word)
 	}
 
 	/*  FIND POSITION OF END OF STREAM  */
-	long end_point = stream.tellp();
+	long end_point = static_cast<long>(stream.tellp());
 
 	/*  CALCULATE HOW MANY CHARACTERS TO SHIFT  */
 	long length = end_point - insert_point;
@@ -1837,7 +1837,7 @@ safety_check(std::stringstream& stream, long* stream_length)
 					break;
 				}
 				if (c == ' ') {
-					last_word_pos = stream.tellg();
+					last_word_pos = static_cast<long>(stream.tellg());
 				}
 			}
 			state = NON_PHONEME;
@@ -1849,7 +1849,7 @@ safety_check(std::stringstream& stream, long* stream_length)
 	}
 
 	/*  BE SURE TO RESET LENGTH OF STREAM  */
-	*stream_length = stream.tellg();
+	*stream_length = static_cast<long>(stream.tellg());
 }
 
 /******************************************************************************
@@ -1907,7 +1907,7 @@ check_tonic(std::stringstream& stream, long start_pos, long end_pos)
 	long i, last_foot_pos = UNDEFINED_POSITION;
 
 	/*  REMEMBER CURRENT POSITION IN STREAM  */
-	long temp_pos = stream.tellp();
+	long temp_pos = static_cast<long>(stream.tellp());
 
 	/*  CALCULATE EXTENT OF STREAM TO LOOP THROUGH  */
 	long extent = end_pos - start_pos;
@@ -2596,7 +2596,7 @@ TextParser::final_conversion(std::stringstream& stream1, long stream1_length,
 
 				/*  SET LAST WRITTEN STATE, AND END POSITION AFTER THE WORD  */
 				last_written_state = STATE_WORD;
-				last_word_end = stream2.tellp();
+				last_word_end = static_cast<long>(stream2.tellp());
 				break;
 
 			case STATE_MEDIAL_PUNC:
@@ -2604,7 +2604,7 @@ TextParser::final_conversion(std::stringstream& stream1, long stream1_length,
 				switch(last_written_state) {
 				case STATE_WORD:
 					if (shift_silence(input, i, stream1_length, mode, stream2)) {
-						last_word_end = stream2.tellp();
+						last_word_end = static_cast<long>(stream2.tellp());
 					} else if ((next_state != STATE_END) &&
 							another_word_follows(input, i, stream1_length, mode)) {
 						if (!strcmp(word,",")) {
@@ -2629,7 +2629,7 @@ TextParser::final_conversion(std::stringstream& stream1, long stream1_length,
 			case STATE_FINAL_PUNC:
 				if (last_written_state == STATE_WORD) {
 					if (shift_silence(input, i, stream1_length, mode, stream2)) {
-						last_word_end = stream2.tellp();
+						last_word_end = static_cast<long>(stream2.tellp());
 						stream2 << TONE_GROUP_BOUNDARY << ' ';
 						prior_tonic = TTS_FALSE;
 						if (set_tone_group(stream2, tg_marker_pos, word) == TTS_PARSER_FAILURE) {
@@ -2669,11 +2669,11 @@ TextParser::final_conversion(std::stringstream& stream1, long stream1_length,
 						return TTS_PARSER_FAILURE;
 					}
 					last_written_state = STATE_SILENCE;
-					last_word_end = stream2.tellp();
+					last_word_end = static_cast<long>(stream2.tellp());
 				} else if (last_written_state == STATE_WORD) {
 					convert_silence(word, stream2);
 					last_written_state = STATE_SILENCE;
-					last_word_end = stream2.tellp();
+					last_word_end = static_cast<long>(stream2.tellp());
 				}
 				break;
 
@@ -2717,7 +2717,7 @@ TextParser::final_conversion(std::stringstream& stream1, long stream1_length,
 	stream2 << '\0';
 
 	/*  SET STREAM2 LENGTH  */
-	*stream2_length = stream2.tellp();
+	*stream2_length = static_cast<long>(stream2.tellp());
 
 	/*  RETURN SUCCESS  */
 	return TTS_PARSER_SUCCESS;
@@ -2840,7 +2840,7 @@ TextParser::expand_word(char* word, int is_tonic, std::stringstream& stream)
 
 	/*  IF TONIC, CONVERT LAST FOOT MARKER TO TONIC MARKER  */
 	if (is_tonic && (last_foot_begin != UNDEFINED_POSITION)) {
-		long temporaryPosition = stream.tellp();
+		long temporaryPosition = static_cast<long>(stream.tellp());
 		stream.seekp(last_foot_begin);
 		stream << TONIC_BEGIN;
 		stream.seekp(temporaryPosition);

src/trm/MovingAverageFilter.h
@@ -19,6 +19,7 @@
 #define MOVING_AVERAGE_FILTER_H_
 
 #include <cassert>
+#include <cstddef> /* std::size_t */
 #include <cmath> /* round */
 #include <vector>
 
@@ -51,7 +52,7 @@ private:
  */
 template<typename FloatType>
 MovingAverageFilter<FloatType>::MovingAverageFilter(FloatType sampleRate, FloatType period)
-		: buf_(std::round(sampleRate * period))
+		: buf_(static_cast<std::size_t>(std::round(sampleRate * period)))
 		, pos_(buf_.size())
 		, sum_(0.0)
 		, invN_(1.0 / buf_.size())

src/trm/NoiseSource.cpp
@@ -46,7 +46,7 @@ double
 NoiseSource::getSample()
 {
 	double product = seed_ * FACTOR;
-	seed_ = product - (int) product;
+	seed_ = product - static_cast<int>(product);
 	return seed_ - 0.5;
 }
 

src/trm/SampleRateConverter.cpp
@@ -309,7 +309,7 @@ SampleRateConverter::dataEmpty()
 			numberSamples_++;
 
 			/*  SAVE THE SAMPLE  */
-			outputData_.push_back(output);
+			outputData_.push_back(static_cast<float>(output));
 
 			/*  CHANGE TIME REGISTER BACK TO ORIGINAL FORM  */
 			timeRegister_ = ~timeRegister_;
@@ -376,7 +376,7 @@ SampleRateConverter::dataEmpty()
 			numberSamples_++;
 
 			/*  SAVE THE SAMPLE  */
-			outputData_.push_back(output);
+			outputData_.push_back(static_cast<float>(output));
 
 			/*  INCREMENT THE TIME REGISTER  */
 			timeRegister_ += timeRegisterIncrement_;

src/trm/Tube.cpp
@@ -515,8 +515,8 @@ Tube::initializeSynthesizer()
 	/*  CALCULATE THE SAMPLE RATE, BASED ON NOMINAL TUBE LENGTH AND SPEED OF SOUND  */
 	if (length_ > 0.0) {
 		double c = speedOfSound(temperature_);
-		controlPeriod_ = rint((c * TOTAL_SECTIONS * 100.0) / (length_ * controlRate_));
-		sampleRate_ = controlRate_ * controlPeriod_;
+		controlPeriod_ = static_cast<int>(rint((c * TOTAL_SECTIONS * 100.0) / (length_ * controlRate_)));
+		sampleRate_ = static_cast<int>(controlRate_ * controlPeriod_);
 		actualTubeLength_ = (c * TOTAL_SECTIONS * 100.0) / sampleRate_;
 		nyquist = sampleRate_ / 2.0;
 	} else {
@@ -1070,7 +1070,7 @@ Tube::writeOutputToBuffer(std::vector<float>& outputBuffer)
 float
 Tube::calculateMonoScale()
 {
-	float scale = (OUTPUT_SCALE / srConv_->maximumSampleValue()) * amplitude(volume_);
+	float scale = static_cast<float>((OUTPUT_SCALE / srConv_->maximumSampleValue()) * amplitude(volume_));
 	LOG_DEBUG("\nScale: " << scale << '\n');
 	return scale;
 }
@@ -1078,10 +1078,10 @@ Tube::calculateMonoScale()
 void
 Tube::calculateStereoScale(float& leftScale, float& rightScale)
 {
-	leftScale = -((balance_ / 2.0) - 0.5);
-	rightScale = ((balance_ / 2.0) + 0.5);
-	float newMax = srConv_->maximumSampleValue() * (balance_ > 0.0 ? rightScale : leftScale);
-	float scale = (OUTPUT_SCALE / newMax) * amplitude(volume_);
+	leftScale = static_cast<float>(-((balance_ / 2.0) - 0.5));
+	rightScale = static_cast<float>(((balance_ / 2.0) + 0.5));
+	float newMax = static_cast<float>(srConv_->maximumSampleValue() * (balance_ > 0.0 ? rightScale : leftScale));
+	float scale = static_cast<float>((OUTPUT_SCALE / newMax) * amplitude(volume_));
 	leftScale  *= scale;
 	rightScale *= scale;
 	LOG_DEBUG("\nLeft scale: " << leftScale << " Right scale: " << rightScale << '\n');

commit 68aef672e34174ab316a4595c67657aae03fa029

Fix intonation. This bug was caused by the commit 7789b795 (2015-08-15).

src/trm_control_model/EventList.cpp
@@ -1026,7 +1026,7 @@ EventList::generateOutput(std::ostream& trmParamStream)
 		}
 		if (!microFlag_) table[0] = 0.0;
 		if (driftFlag_)  table[0] += static_cast<float>(driftGenerator_.drift());
-		if (macroFlag_)  table[0] += static_cast<long>(currentValues[32]);
+		if (macroFlag_)  table[0] += static_cast<float>(currentValues[32]);
 
 		table[0] += static_cast<float>(pitchMean_);
 